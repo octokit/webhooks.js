b6fcbe50a59a20e57581e56f144096f8bff72fc0
Gregor <gregor@martynus.net>
2017-11-27 12:05:20 -0800

    fix: timing attach vulnerability

--- verify/index.js
+++ verify/index.js
@@ -1,6 +1,9 @@
 module.exports = verify
 
+const crypto = require('crypto')
 const Buffer = require('buffer').Buffer
+
+const timingSafeEqualPolyfill = require('buffer-equal-constant-time')
 
 const sign = require('../sign')
 
@@ -9,8 +12,23 @@ function verify (secret, eventPayload, signature) {
     throw new TypeError('secret, eventPayload & signature required')
   }
 
-  return Buffer.compare(
-    Buffer.from(signature),
-    Buffer.from(sign(secret, eventPayload))
-  ) === 0
+  const signatureBuffer = Buffer.from(signature)
+  const verificationBuffer = Buffer.from(sign(secret, eventPayload))
+
+  if (signatureBuffer.length !== verificationBuffer.length) {
+    return false
+  }
+
+  return timingSafeEqual(signatureBuffer, verificationBuffer)
+}
+
+/* istanbul ignore next */
+function timingSafeEqual (signatureBuffer, verificationBuffer) {
+  // crypto.verificationBuffer was added in Node 6.6
+  // https://nodejs.org/docs/latest-v6.x/api/crypto.html#crypto_crypto_timingsafeequal_a_b
+  if ('timingSafeEqual' in crypto) {
+    return crypto.timingSafeEqual(signatureBuffer, verificationBuffer)
+  }
+
+  return timingSafeEqualPolyfill(signatureBuffer, verificationBuffer)
 }
