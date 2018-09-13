d48a53fcfd793591d70d8fd701418d53351826bd
Gregor <gregor@martynus.net>
2018-09-13 11:49:04 -0700

    test: handle invalid JSON in payload

--- test/integration/middleware-test.js
+++ test/integration/middleware-test.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events')
+const Buffer = require('buffer').Buffer
 
 const test = require('tap').test
 const simple = require('simple-mock')
@@ -10,6 +11,28 @@ const headers = {
   'x-github-event': 'push',
   'x-hub-signature': 'sha1=f4d795e69b5d03c139cc6ea991ad3e5762d13e2f'
 }
+
+test('Invalid payload', t => {
+  const requestMock = new EventEmitter()
+  requestMock.method = 'POST'
+  requestMock.headers = headers
+  requestMock.url = '/'
+
+  const responseMock = {
+    end: simple.spy()
+  }
+
+  const middleware = createMiddleware({ secret: 'mysecret' })
+  middleware(requestMock, responseMock)
+
+  requestMock.emit('data', Buffer.from('foo'))
+  requestMock.emit('end')
+
+  t.is(responseMock.statusCode, 400)
+  t.is(responseMock.end.lastCall.arg, 'Invalid JSON')
+
+  t.end()
+})
 
 test('request error', t => {
   const requestMock = new EventEmitter()
