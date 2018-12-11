af37b49bf7941a0588b351ed8ef62aa50143a0b1
Gregor <gregor@martynus.net>
2018-12-11 15:35:32 -0800

    fix: remove obsolete and invalid events & actions

--- scripts/update-known-events.js
+++ scripts/update-known-events.js
@@ -5,13 +5,6 @@ const WEBOOOKS = require('@octokit/webhooks-definitions')
 const newWebhookNames = WEBOOOKS.reduce((list, event) => {
   list.push(event.name, ...event.actions.map(action => `${event.name}.${action}`))
   return list
-}, ['*', 'error'])
+}, ['*', 'error']).sort()
 
-// the documentation at https://developer.github.com/v3/activity/events/types/#labelevent
-// does not include all actions, or the actions are not formatted correctly.
-// So for the time being we merge into the current webhook names to prevent
-// events or actions from being removed
-const currentWebhookNames = require('../lib/webhook-names')
-const webhookNames = new Set(newWebhookNames.concat(currentWebhookNames).sort())
-
-writeFileSync('lib/webhook-names.json', JSON.stringify([...webhookNames], null, 2) + '\n')
+writeFileSync('lib/webhook-names.json', JSON.stringify([...newWebhookNames], null, 2) + '\n')
