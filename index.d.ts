octokit/webhooks.js

commit 79fcec23aad842f9e5107cf136801c6849c4e46c

feat(typescript): definitions for .verify, .verifyAndReceive, .receive, .removeListener, .middleware methods (#55)

index.d.ts
@@ -1,10 +1,14 @@
 // DO NOT EDIT THIS FILE DIRECTLY
 // make edits in scripts/generate-types.js
 
+import http = require("http");
+
 export type Options = {
   secret: string;
   path?: string;
-  transform?: (event: Webhooks.WebhookEvent<T>) => Webhooks.WebhookEvent<T> & { [key: string]: any };
+  transform?: (
+    event: Webhooks.WebhookEvent<T>
+  ) => Webhooks.WebhookEvent<T> & { [key: string]: any };
 };
 
 export type WebhookPayloadWatchSender = {
@@ -3477,8 +3481,12 @@ export namespace Webhooks {
 
     public on(event: "error", callback: (event: Error) => void): void;
     public on(
-      event: "*" | string[],
+      event: "*" | string | string[],
       callback: (event: Webhooks.WebhookEvent<any>) => void
+    ): void;
+    public on(
+      event: "*" | string | string[],
+      callback: (event: Webhooks.WebhookEvent<any>) => Promise<void>
     ): void;
 
     public on(
@@ -3812,5 +3820,30 @@ export namespace Webhooks {
     ): void;
 
     public sign(data: any): string;
+    public verify(eventPayload: any, signature: string): boolean;
+    public verifyAndReceive(options: {
+      id: string;
+      name: string;
+      payload: any;
+      signature: string;
+    }): Promise<void>;
+    public receive(options: {
+      id: string;
+      name: string;
+      payload: any;
+    }): Promise<void>;
+    public removeListener(
+      event: string | string[],
+      callback: (event: Webhooks.WebhookEvent<any>) => void
+    ): void;
+    public removeListener(
+      event: string | string[],
+      callback: (event: Webhooks.WebhookEvent<any>) => Promise<void>
+    ): void;
+    public middleware(
+      request: http.ClientRequest,
+      response: http.ServerResponse,
+      next: (err?: any) => void
+    ): (request: http.IncomingMessage, response: http.ServerResponse) => void;
   }
 }

scripts/generate-types.js
@@ -32,6 +32,8 @@ const definition = `
 // DO NOT EDIT THIS FILE DIRECTLY
 // make edits in scripts/generate-types.js
 
+import http = require('http')
+
 export type Options = {
   secret: string
   path?: string
@@ -53,10 +55,17 @@ export namespace Webhooks {
     constructor (options: Options)
 
     public on (event: 'error', callback: (event: Error) => void): void
-    public on (event: '*' | string[], callback: (event: Webhooks.WebhookEvent<any>) => void): void
+    public on (event: '*' | string | string[], callback: (event: Webhooks.WebhookEvent<any>) => void): void
+    public on (event: '*' | string | string[], callback: (event: Webhooks.WebhookEvent<any>) => Promise<void>): void
     ${signatures.join('\n')}
 
     public sign (data: any): string
+    public verify (eventPayload: any, signature: string): boolean
+    public verifyAndReceive (options: { id: string, name: string, payload: any, signature: string }): Promise<void>
+    public receive (options: { id: string, name: string, payload: any }): Promise<void>
+    public removeListener (event: string | string[], callback: (event: Webhooks.WebhookEvent<any>) => void): void
+    public removeListener (event: string | string[], callback: (event: Webhooks.WebhookEvent<any>) => Promise<void>): void
+    public middleware (request: http.ClientRequest, response: http.ServerResponse, next: (err?: any) => void): (request: http.IncomingMessage, response: http.ServerResponse) => void
   }
 }
 `

commit 90e91f620f4d6f895ceb720331d66355fddc0e73

fix(typescript): Update signature for middleware function
The bug was introduced in #55
Fixes #64

index.d.ts
@@ -3876,10 +3876,10 @@ declare class Webhooks {
     callback: (event: Webhooks.WebhookEvent<any>) => Promise<void>
   ): void;
   public middleware(
-    request: http.ClientRequest,
+    request: http.IncomingMessage,
     response: http.ServerResponse,
     next: (err?: any) => void
-  ): (request: http.IncomingMessage, response: http.ServerResponse) => void;
+  ): void | Promise<void>;
 }
 
 export = Webhooks;