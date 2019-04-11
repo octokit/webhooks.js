27da0003e8d4c08a7129dc43d823ab40c5263bc9
Gregor <gregor@martynus.net>
2019-04-11 09:06:08 -0700

    fix: normalize escape sequences after JSON.stringify()

--- sign/index.js
+++ sign/index.js
@@ -7,6 +7,12 @@ function sign (secret, payload) {
     throw new TypeError('secret & payload required')
   }
 
-  payload = typeof payload === 'string' ? payload : JSON.stringify(payload)
+  payload = typeof payload === 'string' ? payload : toNormalizedJsonString(payload)
   return 'sha1=' + crypto.createHmac('sha1', secret).update(payload).digest('hex')
+}
+
+function toNormalizedJsonString (payload) {
+  return JSON.stringify(payload).replace(/\\u[\da-f]{4}/g, s => {
+    return s.substr(0, 2) + s.substr(2).toUpperCase()
+  })
 }
