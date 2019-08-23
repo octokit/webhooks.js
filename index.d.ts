90697e387c5c053e704a0f905e0580f2f40f9ea3
Gregor Martynus <gregor@martynus.net>
2019-08-23 13:12:57 -0700

    feat: add repository_dispatch event (#92)

--- index.d.ts
+++ index.d.ts
@@ -434,6 +434,53 @@ declare namespace Webhooks {
     repository: PayloadRepository;
     sender: WebhookPayloadRepositorySender;
   };
+  type WebhookPayloadRepositoryDispatchInstallation = {
+    id: number;
+    node_id: string;
+  };
+  type WebhookPayloadRepositoryDispatchSender = {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+  };
+  type WebhookPayloadRepositoryDispatchOrganization = {
+    login: string;
+    id: number;
+    node_id: string;
+    url: string;
+    repos_url: string;
+    events_url: string;
+    hooks_url: string;
+    issues_url: string;
+    members_url: string;
+    public_members_url: string;
+    avatar_url: string;
+    description: string;
+  };
+  type WebhookPayloadRepositoryDispatch = {
+    action: string;
+    branch: string;
+    ref: string;
+    repository: PayloadRepository;
+    organization: WebhookPayloadRepositoryDispatchOrganization;
+    sender: WebhookPayloadRepositoryDispatchSender;
+    installation: WebhookPayloadRepositoryDispatchInstallation;
+  };
   type WebhookPayloadReleaseSender = {
     login: string;
     id: number;
@@ -3761,7 +3808,7 @@ declare namespace Webhooks {
     private: boolean;
     owner: PayloadRepositoryOwner;
     html_url: string;
-    description: null;
+    description: null | string;
     fork: boolean;
     url: string;
     forks_url: string;
@@ -3807,7 +3854,7 @@ declare namespace Webhooks {
     ssh_url: string;
     clone_url: string;
     svn_url: string;
-    homepage: null;
+    homepage: null | string;
     size: number;
     stargazers_count: number;
     watchers_count: number;
@@ -4414,6 +4461,13 @@ declare class Webhooks {
       | "release.unpublished",
     callback: (
       event: Webhooks.WebhookEvent<Webhooks.WebhookPayloadRelease>
+    ) => Promise<void> | void
+  ): void;
+
+  public on(
+    event: "repository_dispatch",
+    callback: (
+      event: Webhooks.WebhookEvent<Webhooks.WebhookPayloadRepositoryDispatch>
     ) => Promise<void> | void
   ): void;
 
