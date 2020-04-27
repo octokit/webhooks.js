bbfcd98d08a22b33967996046032fbfefd8cce3e
Oscar Dominguez <dominguez.celada@gmail.com>
2020-04-27 02:06:30 +0200

    🚨 test(smoke-test): assert new entry-points do not emit a warning when importing them

--- test/integration/smoke-test.js
+++ test/integration/smoke-test.js
@@ -1,6 +1,8 @@
 const test = require("tap").test;
+const { mock } = require("simple-mock");
 
 test("@octokit/webhooks", (t) => {
+  const emitWarningMock = mock(process, "emitWarning");
   const { createWebhooksApi } = require("../../lib");
   const api = createWebhooksApi({ secret: "mysecret" });
 
@@ -11,42 +13,55 @@ test("@octokit/webhooks", (t) => {
   t.type(api.receive, "function");
   t.type(api.middleware, "function");
   t.type(api.verifyAndReceive, "function");
+  t.false(emitWarningMock.called);
 
   t.end();
 });
 
 test('require("@octokit/webhooks").sign', (t) => {
+  const emitWarningMock = mock(process, "emitWarning");
+
   t.doesNotThrow(() => {
     const { sign } = require("../../lib");
     sign("1234", {});
   });
+  t.false(emitWarningMock.called);
 
   t.end();
 });
 
 test('require("@octokit/webhooks").verify', (t) => {
+  const emitWarningMock = mock(process, "emitWarning");
+
   t.doesNotThrow(() => {
     const { verify } = require("../../lib");
     verify("1234", {}, "randomSignature");
   });
+  t.false(emitWarningMock.called);
 
   t.end();
 });
 
 test('require("@octokit/webhooks").createEventHandler', (t) => {
+  const emitWarningMock = mock(process, "emitWarning");
+
   t.doesNotThrow(() => {
     const { createEventHandler } = require("../../lib");
     createEventHandler();
   });
+  t.false(emitWarningMock.called);
 
   t.end();
 });
 
 test('require("@octokit/webhooks).createMiddleware")', (t) => {
+  const emitWarningMock = mock(process, "emitWarning");
+
   t.doesNotThrow(() => {
     const { createMiddleware } = require("../../lib");
     createMiddleware({ secret: "1234" });
   });
+  t.false(emitWarningMock.called);
 
   t.end();
 });
