cbfbfe2632db87038f0693f16ad1aabee5115c5a
wolfy1339 <webmaster@wolfy1339.com>
2020-08-06 13:37:47 -0400

    chore(tests): Convert to typescript and use Jest

---
+++ test/unit/event-handler-receive-test.ts
@@ -0,0 +1,39 @@
+import { receiverHandle as receive } from '../../src/event-handler/receive';
+import { State } from '../../src/types';
+
+const state: State = {
+  secret: 'mysecret',
+  hooks: {}
+};
+
+test('options: none', () => {
+  expect(receive(state)).toThrow();
+
+test('options: name', () => {
+  expect(
+    receive(state, { name: 'foo' })
+  ).toThrow()
+});
+
+test('options: name, payload', () => {
+  expect(
+    receive(state, { name: 'foo', payload: {} })
+  ).not.toThrow();
+});
+
+test('options: [name1, name2]', () => {
+  expect(
+    receive(state, {
+      name: ['foo', 'bar']
+    })
+  ).toThrow();
+});
+
+test('options: [name1, name2], payload', () => {
+  expect(
+    receive(state, {
+      name: ['foo', 'bar'],
+      payload: {}
+    })
+  ).not.toThrow();
+})
--- test/unit/middleware-constructor-test.js
+++
@@ -1,7 +0,0 @@
-import { test } from "tap";
-import { createMiddleware as Middleware } from "../../pkg/dist-src/middleware";
-
-test("options: none", (t) => {
-  t.throws(Middleware);
-  t.end();
-});
