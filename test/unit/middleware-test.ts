ed5b2be9bd3893f50240536b2cccb140e44fbdda
wolfy1339 <webmaster@wolfy1339.com>
2020-08-06 13:37:47 -0400

    chore(tests): Convert to typescript and use Jest

---
+++ test/unit/middleware-test.ts
@@ -0,0 +1,14 @@
+import simple from 'simple-mock';
+import { middleware } from '../../src/middleware/middleware';
+import { State } from '../../src/types';
+
+const state: State = {
+  hooks: {}
+};
+
+test('next() callback', () => {
+  const next = simple.spy();
+
+  middleware(state, { method: 'POST', url: '/foo' }, {}, next);
+  expect(next.callCount).toBe(1);
+});
