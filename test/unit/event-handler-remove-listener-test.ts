7bf6badad4416c7e20bea7e0788f1111291b27ab
wolfy1339 <webmaster@wolfy1339.com>
2020-08-06 13:37:47 -0400

    chore(tests): Convert to typescript and use Jest

---
+++ test/unit/event-handler-remove-listener-test.ts
@@ -0,0 +1,39 @@
+import { removeListener } from '../../src/event-handler/remove-listener';
+import { State } from '../../src/types';
+
+test('remove-listener: single listener', () => {
+  const push = () => {};
+
+  const state: State = {
+    hooks: {
+      push: [push]
+    }
+  };
+
+  expect(
+    removeListener(state, 'push', push)
+  ).not.toThrow();
+  expect(state).toStrictEqual({ hooks: { push: [] } });
+});
+
+test('remove-listener: multiple listeners', () => {
+  const push1 = () => {};
+  const push2 = () => {};
+  const push3 = () => {};
+
+  const ping = () => {};
+
+  const state: State = {
+    hooks: {
+      push: [push1, push2, push3],
+      ping: [ping]
+    }
+  };
+
+  expect(() => {
+    removeListener(state, 'push', push1);
+    removeListener(state, 'push', push2);
+    removeListener(state, 'push', push3);
+  }).not.toThrow();
+  expect(state).toStrictEqual({ hooks: { push: [], ping: [ping] } });
+});
