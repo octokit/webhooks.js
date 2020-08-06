c42117a9b76e01f9e5d24eab278fef93c3a8dc71
wolfy1339 <webmaster@wolfy1339.com>
2020-08-06 13:40:30 -0400

    chore(lint): Run prettier

--- test/unit/event-handler-wrap-error-handler-test.ts
+++ test/unit/event-handler-wrap-error-handler-test.ts
@@ -1,34 +1,31 @@
-import simple from 'simple-mock';
-import { wrapErrorHandler } from '../../src/event-handler/wrap-error-handler';
+import simple from "simple-mock";
+import { wrapErrorHandler } from "../../src/event-handler/wrap-error-handler";
 
-test('error thrown in error handler', t => {
+test("error thrown in error handler", (t) => {
   t.plan(2);
 
   const messages = [];
-  simple.mock(console, 'log', messages.push.bind(messages));
+  simple.mock(console, "log", messages.push.bind(messages));
   expect(
     wrapErrorHandler(() => {
-      throw new Error('oopsydoopsy');
-    }, new Error('oops'))
+      throw new Error("oopsydoopsy");
+    }, new Error("oops"))
   ).not.toThrow();
 
-  t.ok(messages.find(message => /FATAL/.test(message)));
+  t.ok(messages.find((message) => /FATAL/.test(message)));
   simple.restore();
 });
 
-test('error handler returns rejected Error', t => {
+test("error handler returns rejected Error", (t) => {
   t.plan(2);
 
   const messages = [];
-  simple.mock(console, 'log', messages.push.bind(messages));
-  const promise = Promise.reject(new Error('oopsydoopsy'));
-  expect(
-    wrapErrorHandler(() => promise, new Error('oops'))
-  ).not.toThrow();
-
+  simple.mock(console, "log", messages.push.bind(messages));
+  const promise = Promise.reject(new Error("oopsydoopsy"));
+  expect(wrapErrorHandler(() => promise, new Error("oops"))).not.toThrow();
 
   promise.catch(() => {
-    t.ok(messages.find(message => /FATAL/.test(message)));
+    t.ok(messages.find((message) => /FATAL/.test(message)));
     simple.restore();
   });
 });
