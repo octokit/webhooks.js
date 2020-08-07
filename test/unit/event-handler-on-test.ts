a3c8a33f71736c8bb2781d46cda7dc57c6505e9d
wolfy1339 <webmaster@wolfy1339.com>
2020-08-06 23:34:03 -0400

    fix(typescript): Make sure to use the simple-mock types

--- test/unit/event-handler-on-test.ts
+++ test/unit/event-handler-on-test.ts
@@ -11,8 +11,8 @@ const state: State = {
 test("receiver.on with invalid event name", () => {
   simple.mock(console, "warn").callFn(function () {});
   receiverOn(state, "foo", noop);
-  expect(console.warn.callCount).toBe(1);
-  expect(console.warn.lastCall.arg).toBe(
+  expect((console.warn as simple.Stub<void>).callCount).toBe(1);
+  expect((console.warn as simple.Stub<void>).lastCall.arg).toBe(
     '"foo" is not a known webhook name (https://developer.github.com/v3/activity/events/types/)'
   );
 
