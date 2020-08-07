6493dd88d3ce78e42a8ef44f45f80d2f10073ab5
wolfy1339 <webmaster@wolfy1339.com>
2020-08-07 12:38:47 -0400

    fix(tests): Fix some type errors

--- test/unit/middleware-constructor-test.ts
+++ test/unit/middleware-constructor-test.ts
@@ -1,5 +1,5 @@
 import { createMiddleware as Middleware } from "../../src/middleware";
 
 test("options: none", () => {
-  expect(() => Middleware()).toThrow();
+  expect(() => Middleware({})).toThrow();
 });
