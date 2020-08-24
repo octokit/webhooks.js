80481c6ebddf4dc0d4c53d348ca083413af6896f
Oscar Dominguez <dominguez.celada@gmail.com>
2020-08-25 00:40:55 +0200

    test(sign-test): restore coverage 100% for sign-test

--- src/sign/index.ts
+++ src/sign/index.ts
@@ -1,6 +1,6 @@
 import { createHmac } from "crypto";
 
-export function sign(secret: string, payload: string | object): string {
+export function sign(secret?: string, payload?: string | object): string {
   // @ts-ignore throw friendly error message when required options are missing
   if (!secret || !payload) {
     throw new TypeError("secret & payload required");
