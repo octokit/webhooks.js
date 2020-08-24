2e6e19f7f6d358565caf9c77ff858eb70ab7d1f0
Oscar Dominguez <dominguez.celada@gmail.com>
2020-08-25 00:22:26 +0200

    test(sign): restore 100% coverage for sign-test.ts

--- test/integration/sign-test.ts
+++ test/integration/sign-test.ts
@@ -5,6 +5,19 @@ const eventPayload = {
 };
 const secret = "mysecret";
 
+test("sign() without options throws", () => {
+  // @ts-ignore
+  expect(() => sign()).toThrow();
+});
+
+test("sign(undefined, eventPayload) without secret throws", () => {
+  expect(() => sign.bind(null, undefined, eventPayload)()).toThrow();
+});
+
+test("sign(secret) without eventPayload throws", () => {
+  expect(() => sign.bind(null, secret)()).toThrow();
+});
+
 test("sign(secret, eventPayload) with eventPayload as object returns expected signature", () => {
   const signature = sign(secret, eventPayload);
   expect(signature).toBe("sha1=d03207e4b030cf234e3447bac4d93add4c6643d8");
