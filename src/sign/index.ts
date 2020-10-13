cd79a3219b8c401875ad13fe9c37b8a7c5f48a9b
Oscar Dominguez <dominguez.celada@gmail.com>
2020-10-13 19:52:57 +0200

    feat(sign): support sha256 in sign()

--- src/sign/index.ts
+++ src/sign/index.ts
@@ -1,6 +1,29 @@
 import { createHmac } from "crypto";
 
-export function sign(secret: string, payload: string | object): string {
+export enum Algorithm {
+  SHA1 = "sha1",
+  SHA256 = "sha256",
+}
+
+type SignOptions = {
+  secret: string;
+  algorithm?: Algorithm;
+};
+
+export function sign(
+  options: SignOptions | string,
+  payload: string | object
+): string {
+  let secret;
+  let algorithm;
+
+  if (typeof options === "string") {
+    secret = options;
+    algorithm = Algorithm.SHA1;
+  } else {
+    secret = options.secret;
+    algorithm = options.algorithm ?? Algorithm.SHA1;
+  }
   // @ts-ignore throw friendly error message when required options are missing
   if (!secret || !payload) {
     throw new TypeError("secret & payload required");
@@ -8,7 +31,9 @@ export function sign(secret: string, payload: string | object): string {
 
   payload =
     typeof payload === "string" ? payload : toNormalizedJsonString(payload);
-  return "sha1=" + createHmac("sha1", secret).update(payload).digest("hex");
+  return `${algorithm}=${createHmac(algorithm, secret)
+    .update(payload)
+    .digest("hex")}`;
 }
 
 function toNormalizedJsonString(payload: object) {
--- test/integration/sign-test.ts
+++ test/integration/sign-test.ts
@@ -20,12 +20,22 @@ test("sign(secret) without eventPayload throws", () => {
   expect(() => sign.bind(null, secret)()).toThrow();
 });
 
-test("sign(secret, eventPayload) with eventPayload as object returns expected signature", () => {
+test("sign(secret, eventPayload) with eventPayload as object returns expected sha1 signature", () => {
   const signature = sign(secret, eventPayload);
   expect(signature).toBe("sha1=d03207e4b030cf234e3447bac4d93add4c6643d8");
 });
 
-test("sign(secret, eventPayload) with eventPayload as string returns expected signature", () => {
+test("sign(secret, eventPayload) with eventPayload as object returns expected sha256 signature", () => {
+  const signature = sign(secret, eventPayload);
+  expect(signature).toBe("sha256=kljsdfjkldasjfkaejkfwekuweur3298r2398r89fdsuf3");
+});
+
+test("sign(secret, eventPayload) with eventPayload as string returns expected sha1 signature", () => {
   const signature = sign(secret, JSON.stringify(eventPayload));
   expect(signature).toBe("sha1=d03207e4b030cf234e3447bac4d93add4c6643d8");
+});
+
+test("sign(secret, eventPayload) with eventPayload as string returns expected sha256 signature", () => {
+  const signature = sign(secret, JSON.stringify(eventPayload));
+  expect(signature).toBe("sha256=kljsdfjkldasjfkaejkfwekuweur3298r2398r89fdsuf3");
 });
