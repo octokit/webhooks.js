99d7d41e6142a5d08b5655f80f277e5b57a9de24
wolfy1339 <webmaster@wolfy1339.com>
2020-10-13 18:15:41 -0400

    Also use the onError method in the server integration test

--- test/integration/server-test.ts
+++ test/integration/server-test.ts
@@ -142,7 +142,7 @@ describe("server-test", () => {
     });
     const server = http.createServer(api.middleware);
     const errorHandler = jest.fn();
-    api.on("error", errorHandler);
+    api.onError(errorHandler);
 
     promisify(server.listen.bind(server))(availablePort)
 
@@ -177,7 +177,7 @@ describe("server-test", () => {
     });
     const server = http.createServer(api.middleware);
     const errorHandler = jest.fn();
-    api.on("error", errorHandler);
+    api.onError(errorHandler);
 
     promisify(server.listen.bind(server))(availablePort)
 
