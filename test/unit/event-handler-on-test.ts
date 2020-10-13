59b4146162d796b80748290a10f274500a4a62d0
wolfy1339 <webmaster@wolfy1339.com>
2020-10-13 18:05:15 -0400

    Add unit test to test the deprecation

--- test/unit/event-handler-on-test.ts
+++ test/unit/event-handler-on-test.ts
@@ -20,3 +20,21 @@ test("receiver.on with invalid event name", () => {
 
   simple.restore();
 });
+
+test("receiver.on with event name of '*' logs deprecation notice", () => {
+  simple.mock(console, "warn").callFn(function () {});
+  receiverOn(state, "*", noop);
+  expect((console.warn as simple.Stub<void>).callCount).toBe(1);
+  expect((console.warn as simple.Stub<void>).lastCall.arg).toBe(
+    'Using the "*" event with the regular Webhooks.on() function is deprecated. Please use the Webhooks.onAny() method instead'
+  );
+});
+
+test("receiver.on with event name of 'error' logs deprecation notice", () => {
+  simple.mock(console, "warn").callFn(function () {});
+  receiverOn(state, "error", noop);
+  expect((console.warn as simple.Stub<void>).callCount).toBe(1);
+  expect((console.warn as simple.Stub<void>).lastCall.arg).toBe(
+    'Using the "error" event with the regular Webhooks.on() function is deprecated. Please use the Webhooks.onError() method instead'
+  );
+});
