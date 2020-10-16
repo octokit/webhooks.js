ee25931f40b6959f6f3a5312cd611f32c053c52f
Oscar Dominguez <dominguez.celada@gmail.com>
2020-10-16 02:03:52 +0200

    test(verify-test): use right sha256 for test with special characters

--- test/integration/verify-test.ts
+++ test/integration/verify-test.ts
@@ -82,14 +82,14 @@ test("verify(secret, eventPayload, signatureSHA256) returns false for correct se
   expect(signatureMatches).toBe(false);
 });
 
-test("verify(secret, eventPayload, signatureSHA256) returns true if eventPayload contains (#71)", () => {
+test("verify(secret, eventPayload, signatureSHA256) returns true if eventPayload contains special characters (#71)", () => {
   // https://github.com/octokit/webhooks.js/issues/71
   const signatureMatchesLowerCaseSequence = verify(
     "development",
     {
       foo: "Foo\n\u001b[34mbar: ♥♥♥♥♥♥♥♥\nthis-is-lost\u001b[0m\u001b[2K",
     },
-    "sha1=7316ec5e7866e42e4aba4af550d21a5f036f949d"
+    "sha256=afecc3caa27548bb90d51a50384cb2868b9a3327b4ad6a01c9bd4ed0f8b0b12c"
   );
   expect(signatureMatchesLowerCaseSequence).toBe(true);
   const signatureMatchesUpperCaseSequence = verify(
@@ -97,7 +97,7 @@ test("verify(secret, eventPayload, signatureSHA256) returns true if eventPayload
     {
       foo: "Foo\n\u001B[34mbar: ♥♥♥♥♥♥♥♥\nthis-is-lost\u001B[0m\u001B[2K",
     },
-    "sha1=7316ec5e7866e42e4aba4af550d21a5f036f949d"
+    "sha256=afecc3caa27548bb90d51a50384cb2868b9a3327b4ad6a01c9bd4ed0f8b0b12c"
   );
   expect(signatureMatchesUpperCaseSequence).toBe(true);
   const signatureMatchesEscapedSequence = verify(
@@ -105,7 +105,7 @@ test("verify(secret, eventPayload, signatureSHA256) returns true if eventPayload
     {
       foo: "\\u001b",
     },
-    "sha1=2c440a176f4cb84c8c921dfee882d594c2465097"
+    "sha256=6f8326efbacfbd04e870cea25b5652e635be8c9807f2fd5348ef60753c9e96ed"
   );
   expect(signatureMatchesEscapedSequence).toBe(true);
 });
