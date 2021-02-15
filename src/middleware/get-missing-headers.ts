6f0123974521bf172c1499948544b314cbbea023
Jack Bates <jack@nottheoilrig.com>
2021-02-15 14:55:29 -0700

    docs: update broken delivery headers URL in comments (#467)
    Co-authored-by: wolfy1339 <4595477+wolfy1339@users.noreply.github.com>

--- src/middleware/get-missing-headers.ts
+++ src/middleware/get-missing-headers.ts
@@ -6,7 +6,7 @@ const WEBHOOK_HEADERS = [
   "x-github-delivery",
 ];
 
-// https://developer.github.com/webhooks/#delivery-headers
+// https://docs.github.com/en/developers/webhooks-and-events/webhook-events-and-payloads#delivery-headers
 export function getMissingHeaders(request: IncomingMessage) {
   return WEBHOOK_HEADERS.filter((header) => !(header in request.headers));
 }
