65b25b2900aedf4b34866be352e253659aa344ad
wolfy1339 <webmaster@wolfy1339.com>
2021-03-27 17:48:53 -0400

    tests: deprecation notice uses `error` and not `warn`

--- test/unit/deprecation.test.ts
+++ test/unit/deprecation.test.ts
@@ -2,7 +2,7 @@ import { createWebhooksApi } from "../../src";
 
 describe("Deprecated methods", () => {
   test("createWebhooksApi", () => {
-    const spy = jest.spyOn(console, "warn");
+    const spy = jest.spyOn(console, "error");
     createWebhooksApi({ secret: "foo" });
     expect(spy).toBeCalledWith(
       "[@octokit/webhooks] `createWebhooksApi()` is deprecated and will be removed in a future release of `@octokit/webhooks`, please use the `Webhooks` class instead"
