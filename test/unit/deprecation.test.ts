c942c0ba179f0519945cd253fffd39c7ac9ff746
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-30 06:25:51 -0700

    fix: remove deprecated `webhooks.middleware` and undocumented `createMiddleware`
    BREAKING CHANGE: `webhooks.middleware` has been removed. Use `createNodeMiddleware()` instead
    
    BREAKING CHANGE: `createMiddleware` has been removed. Use `createNodeMiddleware()` instead

--- test/unit/deprecation.test.ts
+++ test/unit/deprecation.test.ts
@@ -18,32 +18,4 @@ describe("Deprecated methods", () => {
       "[@octokit/webhooks] `path` option is deprecated and will be removed in a future release of `@octokit/webhooks`. Please use `createNodeMiddleware(webhooks, { path })` instead"
     );
   });
-
-  test("webhooks.middleware", () => {
-    expect.assertions(2);
-
-    const warn = jest.fn();
-    const webhooks = new Webhooks({
-      secret: "secret",
-      log: {
-        debug: () => {},
-        info: () => {},
-        warn: warn,
-        error: () => {},
-      },
-    });
-
-    try {
-      // @ts-expect-error
-      webhooks.middleware();
-    } catch (error) {
-      expect(error.message).toEqual(
-        "Cannot read property 'method' of undefined"
-      );
-    }
-
-    expect(warn).toHaveBeenCalledWith(
-      "[@octokit/webhooks] `webhooks.middleware` is deprecated and will be removed in a future release of `@octokit/webhooks`. Please use `createNodeMiddleware(webhooks)` instead"
-    );
-  });
 });
