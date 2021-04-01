37e6e1e16b14171eb293ad59832d293b0fd716bd
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-04-01 16:34:52 -0700

    docs: add references about why we use `crypto.timingSafeEqual`

--- src/verify/index.ts
+++ src/verify/index.ts
@@ -28,5 +28,8 @@ export function verify(
     return false;
   }
 
+  // constant time comparison to prevent timing attachs
+  // https://stackoverflow.com/a/31096242/206879
+  // https://en.wikipedia.org/wiki/Timing_attack
   return timingSafeEqual(signatureBuffer, verificationBuffer);
 }
