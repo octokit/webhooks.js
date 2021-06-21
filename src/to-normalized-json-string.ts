234c7b4726fee9f742ff21a6645a525bbbe61c82
wolfy1339 <webmaster@wolfy1339.com>
2021-06-21 10:57:02 -0400

    fix: remove indentation and new lines from `toNormalizedJsonString()`

--- src/to-normalized-json-string.ts
+++ src/to-normalized-json-string.ts
@@ -2,7 +2,7 @@
  * GitHub sends its JSON with an indentation of 2 spaces and a line break at the end
  */
 export function toNormalizedJsonString(payload: object) {
-  const payloadString = JSON.stringify(payload, null, 2) + "\n";
+  const payloadString = JSON.stringify(payload);
   return payloadString.replace(/[^\\]\\u[\da-f]{4}/g, (s) => {
     return s.substr(0, 3) + s.substr(3).toUpperCase();
   });
