b09d16484882c7af0d3ba6d9335b33f74658f59d
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2021-07-13 12:28:13 -0400

    feat(typescript): export the `EmitterWebhookEventName` type (#604)

--- src/index.ts
+++ src/index.ts
@@ -64,4 +64,10 @@ class Webhooks<TTransformed = unknown> {
   }
 }
 
-export { createEventHandler, Webhooks, EmitterWebhookEvent, WebhookError };
+export {
+  createEventHandler,
+  Webhooks,
+  EmitterWebhookEvent,
+  EmitterWebhookEventName,
+  WebhookError,
+};
