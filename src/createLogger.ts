555a45ed13011788c9343ab05763aa57bc29a963
m0dul3 <95569820+m0dul3@users.noreply.github.com>
2022-01-03 23:34:17 -0800

    Update createLogger.ts

--- src/createLogger.ts
+++ src/createLogger.ts
@@ -1,14 +1,4 @@
-export interface Logger {
-  debug: (...data: any[]) => void;
-  info: (...data: any[]) => void;
-  warn: (...data: any[]) => void;
-  error: (...data: any[]) => void;
-}
 
-export const createLogger = (logger?: Partial<Logger>): Logger => ({
-  debug: () => {},
-  info: () => {},
-  warn: console.warn.bind(console),
   error: console.error.bind(console),
   ...logger,
 });
