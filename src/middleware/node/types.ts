cf16ae8c79f5b195baf026920c8324c2ab3c83a5
Baoshan Sheng <sheng@icmd.org>
2022-09-12 11:22:06 +0800

    refactor(middleware): remove unused type declarations

--- src/middleware/node/types.ts
+++ src/middleware/node/types.ts
@@ -1,9 +1,3 @@
-// remove type imports from http for Deno compatibility
-// see https://github.com/octokit/octokit.js/issues/24#issuecomment-817361886
-// import { IncomingMessage, ServerResponse } from "http";
-type IncomingMessage = any;
-type ServerResponse = any;
-
 import { Logger } from "../../createLogger";
 
 export type MiddlewareOptions = {
