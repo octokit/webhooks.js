9317e365ed533e0a11f5e923d268dbfde7667ecd
wolfy1339 <webmaster@wolfy1339.com>
2023-05-21 11:13:53 -0400

    build: add script to update generated files + readme

--- src/generated/webhook-identifiers.ts
+++ src/generated/webhook-identifiers.ts
@@ -1,3 +1,6 @@
+// THIS FILE IS GENERATED - DO NOT EDIT DIRECTLY
+// make edits in scripts/generate-types.ts
+
 import { WebhookEventDefinition } from "../types";
 
 export type webhooksIdentifiers = {
@@ -23,6 +26,8 @@ export type webhooksIdentifiers = {
     | WebhookEventDefinition<"code-scanning-alert-reopened-by-user">;
   commit_comment: WebhookEventDefinition<"commit-comment-created">;
   dependabot_alert:
+    | WebhookEventDefinition<"dependabot-alert-auto-dismissed">
+    | WebhookEventDefinition<"dependabot-alert-auto-reopened">
     | WebhookEventDefinition<"dependabot-alert-created">
     | WebhookEventDefinition<"dependabot-alert-dismissed">
     | WebhookEventDefinition<"dependabot-alert-fixed">
@@ -32,16 +37,19 @@ export type webhooksIdentifiers = {
     | WebhookEventDefinition<"deploy-key-created">
     | WebhookEventDefinition<"deploy-key-deleted">;
   deployment: WebhookEventDefinition<"deployment-created">;
+  deployment_protection_rule: WebhookEventDefinition<"deployment-protection-rule-requested">;
   deployment_status: WebhookEventDefinition<"deployment-status-created">;
   discussion:
     | WebhookEventDefinition<"discussion-answered">
     | WebhookEventDefinition<"discussion-category-changed">
+    | WebhookEventDefinition<"discussion-closed">
     | WebhookEventDefinition<"discussion-created">
     | WebhookEventDefinition<"discussion-deleted">
     | WebhookEventDefinition<"discussion-edited">
     | WebhookEventDefinition<"discussion-labeled">
     | WebhookEventDefinition<"discussion-locked">
     | WebhookEventDefinition<"discussion-pinned">
+    | WebhookEventDefinition<"discussion-reopened">
     | WebhookEventDefinition<"discussion-transferred">
     | WebhookEventDefinition<"discussion-unanswered">
     | WebhookEventDefinition<"discussion-unlabeled">
@@ -100,7 +108,9 @@ export type webhooksIdentifiers = {
   membership:
     | WebhookEventDefinition<"membership-added">
     | WebhookEventDefinition<"membership-removed">;
-  merge_group: WebhookEventDefinition<"merge-group-checks-requested">;
+  merge_group:
+    | WebhookEventDefinition<"merge-group-checks-requested">
+    | WebhookEventDefinition<"merge-group-destroyed">;
   meta: WebhookEventDefinition<"meta-deleted">;
   milestone:
     | WebhookEventDefinition<"milestone-closed">
@@ -120,23 +130,34 @@ export type webhooksIdentifiers = {
   package:
     | WebhookEventDefinition<"package-published">
     | WebhookEventDefinition<"package-updated">;
-  project:
-    | WebhookEventDefinition<"project-closed">
-    | WebhookEventDefinition<"project-created">
-    | WebhookEventDefinition<"project-deleted">
-    | WebhookEventDefinition<"project-edited">
-    | WebhookEventDefinition<"project-reopened">;
+  personal_access_token_request:
+    | WebhookEventDefinition<"personal-access-token-request-approved">
+    | WebhookEventDefinition<"personal-access-token-request-cancelled">
+    | WebhookEventDefinition<"personal-access-token-request-created">
+    | WebhookEventDefinition<"personal-access-token-request-denied">;
   project_card:
     | WebhookEventDefinition<"project-card-converted">
     | WebhookEventDefinition<"project-card-created">
     | WebhookEventDefinition<"project-card-deleted">
     | WebhookEventDefinition<"project-card-edited">
     | WebhookEventDefinition<"project-card-moved">;
+  project:
+    | WebhookEventDefinition<"project-closed">
+    | WebhookEventDefinition<"project-created">
+    | WebhookEventDefinition<"project-deleted">
+    | WebhookEventDefinition<"project-edited">
+    | WebhookEventDefinition<"project-reopened">;
   project_column:
     | WebhookEventDefinition<"project-column-created">
     | WebhookEventDefinition<"project-column-deleted">
     | WebhookEventDefinition<"project-column-edited">
     | WebhookEventDefinition<"project-column-moved">;
+  projects_v2:
+    | WebhookEventDefinition<"projects-v2-closed">
+    | WebhookEventDefinition<"projects-v2-created">
+    | WebhookEventDefinition<"projects-v2-deleted">
+    | WebhookEventDefinition<"projects-v2-edited">
+    | WebhookEventDefinition<"projects-v2-reopened">;
   projects_v2_item:
     | WebhookEventDefinition<"projects-v2-item-archived">
     | WebhookEventDefinition<"projects-v2-item-converted">
@@ -154,29 +175,30 @@ export type webhooksIdentifiers = {
     | WebhookEventDefinition<"pull-request-demilestoned">
     | WebhookEventDefinition<"pull-request-dequeued">
     | WebhookEventDefinition<"pull-request-edited">
+    | WebhookEventDefinition<"pull-request-enqueued">
     | WebhookEventDefinition<"pull-request-labeled">
     | WebhookEventDefinition<"pull-request-locked">
+    | WebhookEventDefinition<"pull-request-milestoned">
     | WebhookEventDefinition<"pull-request-opened">
     | WebhookEventDefinition<"pull-request-ready-for-review">
     | WebhookEventDefinition<"pull-request-reopened">
-    | WebhookEventDefinition<"pull-request-review-requested">
     | WebhookEventDefinition<"pull-request-review-request-removed">
+    | WebhookEventDefinition<"pull-request-review-requested">
     | WebhookEventDefinition<"pull-request-synchronize">
     | WebhookEventDefinition<"pull-request-unassigned">
     | WebhookEventDefinition<"pull-request-unlabeled">
     | WebhookEventDefinition<"pull-request-unlocked">;
-  pull_request_review:
-    | WebhookEventDefinition<"pull-request-review-dismissed">
-    | WebhookEventDefinition<"pull-request-review-edited">
-    | WebhookEventDefinition<"pull-request-review-submitted">;
   pull_request_review_comment:
     | WebhookEventDefinition<"pull-request-review-comment-created">
     | WebhookEventDefinition<"pull-request-review-comment-deleted">
     | WebhookEventDefinition<"pull-request-review-comment-edited">;
+  pull_request_review:
+    | WebhookEventDefinition<"pull-request-review-dismissed">
+    | WebhookEventDefinition<"pull-request-review-edited">
+    | WebhookEventDefinition<"pull-request-review-submitted">;
   pull_request_review_thread:
     | WebhookEventDefinition<"pull-request-review-thread-resolved">
     | WebhookEventDefinition<"pull-request-review-thread-unresolved">;
-  push: WebhookEventDefinition<"push">;
   registry_package:
     | WebhookEventDefinition<"registry-package-published">
     | WebhookEventDefinition<"registry-package-updated">;
@@ -188,6 +210,9 @@ export type webhooksIdentifiers = {
     | WebhookEventDefinition<"release-published">
     | WebhookEventDefinition<"release-released">
     | WebhookEventDefinition<"release-unpublished">;
+  repository_advisory:
+    | WebhookEventDefinition<"repository-advisory-published">
+    | WebhookEventDefinition<"repository-advisory-reported">;
   repository:
     | WebhookEventDefinition<"repository-archived">
     | WebhookEventDefinition<"repository-created">
@@ -198,6 +223,7 @@ export type webhooksIdentifiers = {
     | WebhookEventDefinition<"repository-renamed">
     | WebhookEventDefinition<"repository-transferred">
     | WebhookEventDefinition<"repository-unarchived">;
+  repository_dispatch: WebhookEventDefinition<"repository-dispatch-sample.collected">;
   repository_vulnerability_alert:
     | WebhookEventDefinition<"repository-vulnerability-alert-create">
     | WebhookEventDefinition<"repository-vulnerability-alert-dismiss">
@@ -206,7 +232,9 @@ export type webhooksIdentifiers = {
   secret_scanning_alert:
     | WebhookEventDefinition<"secret-scanning-alert-created">
     | WebhookEventDefinition<"secret-scanning-alert-reopened">
-    | WebhookEventDefinition<"secret-scanning-alert-resolved">;
+    | WebhookEventDefinition<"secret-scanning-alert-resolved">
+    | WebhookEventDefinition<"secret-scanning-alert-revoked">;
+  secret_scanning_alert_location: WebhookEventDefinition<"secret-scanning-alert-location-created">;
   security_advisory:
     | WebhookEventDefinition<"security-advisory-published">
     | WebhookEventDefinition<"security-advisory-updated">
@@ -231,7 +259,8 @@ export type webhooksIdentifiers = {
   workflow_job:
     | WebhookEventDefinition<"workflow-job-completed">
     | WebhookEventDefinition<"workflow-job-in-progress">
-    | WebhookEventDefinition<"workflow-job-queued">;
+    | WebhookEventDefinition<"workflow-job-queued">
+    | WebhookEventDefinition<"workflow-job-waiting">;
   workflow_run:
     | WebhookEventDefinition<"workflow-run-completed">
     | WebhookEventDefinition<"workflow-run-in-progress">
