7926f3230b40695d14ede591d0f6576a8eec5aba
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2023-11-26 16:25:25 -0500

    refactor: replace NodeJS internal module imports with `node:` specifier imports

--- src/middleware/node/middleware.ts
+++ src/middleware/node/middleware.ts
@@ -1,6 +1,6 @@
 // remove type imports from http for Deno compatibility
 // see https://github.com/octokit/octokit.js/issues/2075#issuecomment-817361886
-// import { IncomingMessage, ServerResponse } from "http";
+// import { IncomingMessage, ServerResponse } from "node:http";
 type IncomingMessage = any;
 type ServerResponse = any;
 
