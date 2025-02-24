77f1fc65c4dd10a6af38652e44d5471c23e93b84
René <contact.9a5d6388@renegade334.me.uk>
2025-02-24 23:33:14 +0000

    fix(types): explicit return type from web `middleware()`

--- src/middleware/web/middleware.ts
+++ src/middleware/web/middleware.ts
@@ -11,7 +11,7 @@ export async function middleware(
   webhooks: Webhooks,
   options: Required<MiddlewareOptions>,
   request: Request,
-) {
+): Promise<Response> {
   let pathname: string;
   try {
     pathname = new URL(request.url as string, "http://localhost").pathname;
