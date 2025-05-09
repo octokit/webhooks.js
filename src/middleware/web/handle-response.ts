63960ec80be14fdd5a06ff520bf5736d82900611
Aras Abbasi <aras.abbasi@googlemail.com>
2025-05-09 22:07:42 +0200

    fix linting

--- src/middleware/web/handle-response.ts
+++ src/middleware/web/handle-response.ts
@@ -3,13 +3,12 @@ export function handleResponse(
   status = 200 as number,
   headers = {} as Record<string, string>,
 ) {
-
   if (body !== null) {
     headers["content-length"] = body.length.toString();
   }
 
   return new Response(body, {
     status,
-    headers
+    headers,
   });
 }
