67cb3b6e84dba07f8c9f06e4d34d9b93b7ac3025
Aras Abbasi <aras.abbasi@googlemail.com>
2025-06-29 16:05:21 +0200

    use already existing EmitterWebhookEventName

--- src/event-handler/validate-event-name.ts
+++ src/event-handler/validate-event-name.ts
@@ -1,7 +1,6 @@
 import type { Logger } from "../create-logger.ts";
 import { emitterEventNames } from "../generated/webhook-names.ts";
-
-type EventName = (typeof emitterEventNames)[number];
+import type { EmitterWebhookEventName } from "../types.ts";
 
 type ValidateEventNameOptions =
   | {
@@ -15,11 +14,11 @@ type ValidateEventNameOptions =
 export function validateEventName<
   O extends ValidateEventNameOptions = ValidateEventNameOptions,
 >(
-  eventName: EventName | (string & Record<never, never>),
+  eventName: EmitterWebhookEventName | (string & Record<never, never>),
   options: O = {} as O,
 ): asserts eventName is O extends { onUnknownEventName: "warn" }
   ? Exclude<string, "*" | "error">
-  : EventName {
+  : EmitterWebhookEventName {
   if (typeof eventName !== "string") {
     throw new TypeError("eventName must be of type string");
   }
@@ -34,7 +33,7 @@ export function validateEventName<
     );
   }
 
-  if (!emitterEventNames.includes(eventName as EventName)) {
+  if (!emitterEventNames.includes(eventName as EmitterWebhookEventName)) {
     if (options.onUnknownEventName !== "warn") {
       throw new TypeError(
         `"${eventName}" is not a known webhook name (https://developer.github.com/v3/activity/events/types/)`,
